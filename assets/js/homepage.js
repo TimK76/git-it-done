b73c18f416a833d3e3029b98c5c38b7fee8437db
https://uconn.bootcampcontent.com/theknightmoores-xJPRH <theknightmoores@gmail.com>
2022-03-07 16:40:54 -0500

    added JS, HTML, CSS search buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,38 @@
+var userFormEl = document.querySelector("#user-form");
+var languageButtonsEl = document.querySelector("#language-buttons");
+var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
-var userFormEl = document.querySelector("#user-form");
-var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function (event) {
+  // prevent page from refreshing
+  event.preventDefault();
+
+  // get value from input element
+  var username = nameInputEl.value.trim();
+
+  if (username) {
+    getUserRepos(username);
+
+    // clear old content
+    repoContainerEl.textContent = "";
+    nameInputEl.value = "";
+  } else {
+    alert("Please enter a GitHub username");
+  }
+};
+
+var buttonClickHandler = function(event) {
+  // get the language attribute from the clicked element
+  var language = event.target.getAttribute("data-language");
+
+  if (language) {
+    getFeaturedRepos(language);
+
+    // clear old content
+    repoContainerEl.textContent = "";
+  }
+};
 
 var getUserRepos = function (user) {
   //format the github api url
@@ -26,22 +57,21 @@ var getUserRepos = function (user) {
     });
 };
 
-var formSubmitHandler = function (event) {
-  // prevent page from refreshing
-  event.preventDefault();
+var getFeaturedRepos = function (language) {
+  // format the github api url 
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
 
-  // get value from input element
-  var username = nameInputEl.value.trim();
-
-  if (username) {
-    getUserRepos(username);
-
-    // clear old content
-    repoContainerEl.textContent = "";
-    nameInputEl.value = "";
-  } else {
-    alert("Please enter a GitHub username");
-  }
+  // make a get request to url
+  fetch(apiUrl).then(function (response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert('Error: GitHub User Not Found');
+    }
+  });
 };
 
 var displayRepos = function (repos, searchTerm) {
@@ -89,21 +119,6 @@ var displayRepos = function (repos, searchTerm) {
     repoContainerEl.appendChild(repoEl);
   }
 };
-
-var getFeaturedRepos = function (language) {
-  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
-
-  fetch(apiUrl).then(function (response) {
-    if (response.ok) {
-      response.json().then(function(data) {
-        displayRepos(data.items, language);
-      });
-    } else {
-      alert('Error: GitHub User Not Found');
-    }
-  });
-};
-
-
 // add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
